3917b8da541cb8b0c6dfbcbf9dc697d29884539d
PongWasu <89587253+PongWasu@users.noreply.github.com>
2021-09-21 17:06:10 +0700

    Update DontUseReplce.js

--- DontUseReplce.js
+++ DontUseReplce.js
@@ -1,16 +1,18 @@
 function rub(str, chr){
-    let str1= str.split("");
-    let str2= chr.split("");
+    let str1= str;
+    let str2= chr;
+    let str3="";
     
-    for(let i=0; i<str1.length; i++){
-        for(let a=0; a<str2.length; a++){
-            if(str1[i]==str2[a]){
-                str1.splice(i,1);
+    for(let i=0; i<str2.length; i++){
+        for(let a=0; a<str1.length; a++){
+            if(str1[a]!=str2[i]){
+            str3 = str3+str1.charAt(a);
             }
         }
-        
+        str1 = str3;
+        str3 ="";
     }
-    console.log(str1.join(""));
+    console.log(str1);
 
 }
 rub ("This is Thailand", "ai");
